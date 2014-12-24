a59ecd0fdb107c906ac2a8be5b90830b1f4d6b94
David Li <li.davidm96@gmail.com>
2014-12-23 18:07:18 -0700

    Use 4-space indents

--- entity.c
+++ entity.c
@@ -7,122 +7,122 @@
  * Returns: index to the entity, or -1 if no more entities can be created.
  */
 int Entity_New(World *world) {
-  for (int i = 0; i < ENTITY_COUNT; i++) {
-    if (world->mask[i] == NONE) {
-      return i;
+    for (int i = 0; i < ENTITY_COUNT; i++) {
+        if (world->mask[i] == NONE) {
+            return i;
+        }
     }
-  }
-  return -1;
+    return -1;
 }
 
 // Creates a default position (e.g: 0,0) for the given entity
 void Position_New(World *world, int entity) {
-  world->mask[entity] |= POSITION;
-  world->position[entity] = (Position) { 0, 0 };
+    world->mask[entity] |= POSITION;
+    world->position[entity] = (Position) { 0, 0 };
 }
 
 // Creates a new sprite sheet for the given entity
 void Sprite_New(World *world, int entity,
                 int width, int height,
                 int frameWidth, int frameHeight, int frames) {
-  world->mask[entity] |= SPRITE;
-  world->sprite[entity] = (Sprite) {
-    width,
-    height,
-    NULL,
-    frameWidth,
-    frameHeight,
-    frames,
-    0,
-    NULL
-  };
-  world->sprite[entity].frames = malloc(frames * sizeof(SDL_Rect));
-
-  // precalculate rects for frames
-  for (int i = 0; i < frames; i++) {
-    world->sprite[entity].frames[i] = (SDL_Rect) {
-      i * frameWidth,
-      i * frameHeight,
-      frameWidth,
-      frameHeight
+    world->mask[entity] |= SPRITE;
+    world->sprite[entity] = (Sprite) {
+        width,
+        height,
+        NULL,
+        frameWidth,
+        frameHeight,
+        frames,
+        0,
+        NULL
     };
-  }
+    world->sprite[entity].frames = malloc(frames * sizeof(SDL_Rect));
+
+    // precalculate rects for frames
+    for (int i = 0; i < frames; i++) {
+        world->sprite[entity].frames[i] = (SDL_Rect) {
+            i * frameWidth,
+            i * frameHeight,
+            frameWidth,
+            frameHeight
+        };
+    }
 }
 
 void Sprite_NewFromTexture(World *world, int entity,
                            int width, int height,
                            SDL_Texture *texture,
                            int frameWidth, int frameHeight, int frames) {
-  Sprite_New(world, entity, width, height, frameWidth, frameHeight, frames);
-  world->sprite[entity].texture = texture;
+    Sprite_New(world, entity, width, height, frameWidth, frameHeight, frames);
+    world->sprite[entity].texture = texture;
 }
 
 // To David: Why does this take entity as a parameter?
 void Text_New(World *world, int entity, char* text, SDL_Color color) {
-  SDL_Surface *surface = TTF_RenderText_Blended(world->font, text, color);
+    SDL_Surface *surface = TTF_RenderText_Blended(world->font, text, color);
 
-  if (surface == NULL) {
-    printf("%d Couldn't render text! Error: %s\n", __LINE__, TTF_GetError());
-    // end program
-  }
+    if (surface == NULL) {
+        printf("%d Couldn't render text! Error: %s\n", __LINE__, TTF_GetError());
+        // end program
+    }
 
-  SDL_Texture *texture =
-    SDL_CreateTextureFromSurface(world->renderer, surface);
-  if (texture == NULL) {
-    printf("Couldn't render text! Error: %s\n", SDL_GetError());
-    // TODO: handle error
-  }
-  Sprite_New(world, entity, surface->w, surface->h, surface->w, surface->h, 1);
-  world->sprite[entity].texture = texture;
+    SDL_Texture *texture =
+        SDL_CreateTextureFromSurface(world->renderer, surface);
+    if (texture == NULL) {
+        printf("Couldn't render text! Error: %s\n", SDL_GetError());
+        // TODO: handle error
+    }
+    Sprite_New(world, entity, surface->w, surface->h, surface->w, surface->h, 1);
+    world->sprite[entity].texture = texture;
 
-  SDL_FreeSurface(surface);
+    SDL_FreeSurface(surface);
 }
 
 void Position_SetX(World *world, int entity, int x) {
-  world->position[entity].x = x;
+    world->position[entity].x = x;
 }
 
 void Position_SetY(World *world, int entity, int y) {
-  world->position[entity].y = y;
+    world->position[entity].y = y;
 }
 
 void Position_SetXY(World *world, int entity, int x, int y) {
-  world->position[entity].x = x;
-  world->position[entity].y = y;
+    world->position[entity].x = x;
+    world->position[entity].y = y;
 }
 
 void World_Free(World *world) {
-  TTF_CloseFont(world->font);
-  SDL_DestroyRenderer(world->renderer);
-  cpSpaceFree(world->space);
-  for (int i = 0; i < ENTITY_COUNT; i++) {
-    Entity_Free(world, i);
-  }
-  free(world);
+    TTF_CloseFont(world->font);
+    SDL_DestroyRenderer(world->renderer);
+    cpSpaceFree(world->space);
+    for (int i = 0; i < ENTITY_COUNT; i++) {
+        Entity_Free(world, i);
+    }
+    free(world);
 }
 
 void Entity_Free(World *world, int entity)	{
-  if (entity >= ENTITY_COUNT) return;
+    if (entity >= ENTITY_COUNT) return;
 
-  // Free each component of entity. Remember to add when more components
-  // get added!
-  if (world->mask[entity] & SPRITE)	{
-    SDL_DestroyTexture(world->sprite[entity].texture);
-    free(world->sprite[entity].frames);
-  }
-  if (world->mask[entity] & BODY)	cpBodyFree(world->body[entity]);
+    // Free each component of entity. Remember to add when more components
+    // get added!
+    if (world->mask[entity] & SPRITE)	{
+        SDL_DestroyTexture(world->sprite[entity].texture);
+        free(world->sprite[entity].frames);
+    }
+    if (world->mask[entity] & BODY)	cpBodyFree(world->body[entity]);
 }
 
 bool Sprite_HitTest(World *world, int entity, int x, int y) {
-  int entityX = world->position[entity].x;
-  int entityY = world->position[entity].y;
+    int entityX = world->position[entity].x;
+    int entityY = world->position[entity].y;
 
-  int entityWidth = world->sprite[entity].width;
-  int entityHeight = world->sprite[entity].height;
+    int entityWidth = world->sprite[entity].width;
+    int entityHeight = world->sprite[entity].height;
 
-  if (x > entityX && x < (entityX + entityWidth) &&
-      y > entityY && y < (entityY + entityHeight)) {
-    return true;
-  }
-  return false;
+    if (x > entityX && x < (entityX + entityWidth) &&
+        y > entityY && y < (entityY + entityHeight)) {
+        return true;
+    }
+    return false;
 }
--- screens.c
+++ screens.c
@@ -14,110 +14,116 @@
   - Rendering: we need to handle z-ordering somehow. Also scanning every
     entity on every frame is rather wasteful
 
-    For rendering: have a separate renderer that is given pointers to the
-    relevant components
- */
+  For rendering: have a separate renderer that is given pointers to the
+  relevant components
+*/
 bool Main_Init(Game *game) {
-  bool error = false;
-  int bg, title, continueGame, quit;
-  SDL_Surface *surface = NULL;
-  SDL_Texture *texture = NULL;
+    bool error = false;
+    int bg, title, continueGame, quit;
+    SDL_Surface *surface = NULL;
+    SDL_Texture *texture = NULL;
 
-  bg = Entity_New(game->world);
-  Position_New(game->world, bg);
-  Position_SetXY(game->world, bg, 0, 0);
-  surface = IMG_Load("bg.png");
-  if (surface == NULL) {
-    printf("%d Could not load background image: %s\n", __LINE__, IMG_GetError());
-    goto error;
-  }
-  texture = SDL_CreateTextureFromSurface(game->world->renderer, surface);
-  if (texture == NULL) {
-    printf("%d Could not load create background texture: %s\n", __LINE__, SDL_GetError());
-    goto error;
-  }
+    bg = Entity_New(game->world);
+    Position_New(game->world, bg);
+    Position_SetXY(game->world, bg, 0, 0);
+    surface = IMG_Load("bg.png");
+    if (surface == NULL) {
+        printf("%d Could not load background image: %s\n", __LINE__, IMG_GetError());
+        goto error;
+    }
+    texture = SDL_CreateTextureFromSurface(game->world->renderer, surface);
+    if (texture == NULL) {
+        printf("%d Could not load create background texture: %s\n", __LINE__, SDL_GetError());
+        goto error;
+    }
 
-  Sprite_NewFromTexture(game->world, bg, WINDOW_WIDTH, WINDOW_HEIGHT, texture, surface->w, surface->h, 1);
+    Sprite_NewFromTexture(game->world, bg, WINDOW_WIDTH, WINDOW_HEIGHT, texture, surface->w, surface->h, 1);
 
-  title = Entity_New(game->world);
-  Position_New(game->world, title);
-  Position_SetXY(game->world, title, 720, 151);
-  Text_New(game->world, title, "Magical Girl Michael Mauer", (SDL_Color) { 200, 0, 0, 200 });
+    title = Entity_New(game->world);
+    Position_New(game->world, title);
+    Position_SetXY(game->world, title, 720, 151);
+    Text_New(game->world, title, "Magical Girl Michael Mauer", (SDL_Color) { 200, 0, 0, 200 });
 
-  continueGame = Entity_New(game->world);
-  Position_New(game->world, continueGame);
-  Position_SetXY(game->world, continueGame, 720, 250);
-  Text_New(game->world, continueGame, "Continue", (SDL_Color) { 0, 0, 0, 200 });
+    continueGame = Entity_New(game->world);
+    Position_New(game->world, continueGame);
+    Position_SetXY(game->world, continueGame, 720, 250);
+    Text_New(game->world, continueGame, "Continue", (SDL_Color) { 0, 0, 0, 200 });
 
-  quit = Entity_New(game->world);
-  Position_New(game->world, quit);
-  Position_SetXY(game->world, quit, 720, 350);
-  Text_New(game->world, quit, "Quit", (SDL_Color) { 0, 0, 0, 200 });
+    quit = Entity_New(game->world);
+    Position_New(game->world, quit);
+    Position_SetXY(game->world, quit, 720, 350);
+    Text_New(game->world, quit, "Quit", (SDL_Color) { 0, 0, 0, 200 });
 
-  game->screen->update = Main_Update;
-  game->screen->handleEvent = Main_HandleEvent;
-  game->screen->render = Main_Render;
-  game->screen->end = Main_End;
+    game->screen->update = Main_Update;
+    game->screen->handleEvent = Main_HandleEvent;
+    game->screen->render = Main_Render;
+    game->screen->end = Main_End;
 
-  game->screen->state = malloc(sizeof(MainState));
-  ((MainState*) game->screen->state)->continueEntity = continueGame;
-  ((MainState*) game->screen->state)->quitEntity = quit;
+    game->screen->state = malloc(sizeof(MainState));
+    ((MainState*) game->screen->state)->continueEntity = continueGame;
+    ((MainState*) game->screen->state)->quitEntity = quit;
 
-  goto cleanup;
- error:
-  error = true;
- cleanup:
-  SDL_FreeSurface(surface);
+    goto cleanup;
+error:
+    error = true;
+cleanup:
+    SDL_FreeSurface(surface);
 
-  return !error;
+    return !error;
 }
 
 void Main_Update(Game *game) {
 }
 
 void Main_HandleEvent(Game *game, SDL_Event *event) {
-  if (event->type == SDL_MOUSEBUTTONUP) {
-    int x, y;
-    SDL_GetMouseState(&x, &y);
+    if (event->type == SDL_MOUSEBUTTONUP) {
+        int x, y;
+        SDL_GetMouseState(&x, &y);
 
-    if (Sprite_HitTest(game->world, ((MainState*) game->screen->state)->continueEntity, x, y)) {
-      printf("%s\n", "Clicked continue");
-      game->newScreen = Level_Init;
+        if (Sprite_HitTest(game->world, ((MainState*) game->screen->state)->continueEntity, x, y)) {
+            printf("%s\n", "Clicked continue");
+            game->newScreen = Level_Init;
+        }
     }
-  }
 }
 
 void Main_Render(Game *game) {
-  for (int i = 0; i < ENTITY_COUNT; i++) {
-    if (game->world->mask[i] & SPRITE) {
-      int frame = game->world->sprite[i].curFrame;
-      Position position = game->world->position[i];
-      Sprite sprite = game->world->sprite[i];
-      SDL_Rect target = {
-        position.x,
-        position.y,
-        sprite.frameWidth,
-        sprite.frameHeight,
-      };
-      SDL_RenderCopy(game->world->renderer, sprite.texture,
-                     &game->world->sprite[i].frames[frame],
-                     &target);
+    for (int i = 0; i < ENTITY_COUNT; i++) {
+        if (game->world->mask[i] & SPRITE) {
+            int frame = game->world->sprite[i].curFrame;
+            Position position = game->world->position[i];
+            Sprite sprite = game->world->sprite[i];
+            SDL_Rect target = {
+                position.x,
+                position.y,
+                sprite.frameWidth,
+                sprite.frameHeight,
+            };
+            SDL_RenderCopy(game->world->renderer, sprite.texture,
+                           &game->world->sprite[i].frames[frame],
+                           &target);
+        }
     }
-  }
 }
 
 void Main_End(Game *game) {
-  free(game->screen->state);
+    free(game->screen->state);
 }
 
 bool Level_Init(Game *game) {
-  game->screen->update = Level_Update;
-  game->screen->handleEvent = Level_HandleEvent;
-  game->screen->render = Level_Render;
-  game->screen->end = Level_End;
+    game->screen->update = Level_Update;
+    game->screen->handleEvent = Level_HandleEvent;
+    game->screen->render = Level_Render;
+    game->screen->end = Level_End;
 
-  game->screen->state = malloc(sizeof(LevelState));
-  return true;
+    game->screen->state = malloc(sizeof(LevelState));
+    LevelState *state = (LevelState*) game->screen->state;
+    state->map = tmx_load("map.tmx");
+    if (state->map == NULL) {
+        printf("%d Could not load map: %s\n", __LINE__, tmx_strerr());
+        return false;
+    }
+    return true;
 }
 
 void Level_HandleEvent(Game *game, SDL_Event *event) {
@@ -130,5 +136,6 @@ void Level_Render(Game *game) {
 }
 
 void Level_End(Game *game) {
-  free(game->screen->state);
+    tmx_map_free(((LevelState*) game->screen->state)->map);
+    free(game->screen->state);
 }
